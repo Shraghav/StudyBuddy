422b0d0a06cda2f7c4e143a87a294c41a9afc2cd
Shraghav <harishragavan04@gmail.com>
2026-03-12 23:10:21 +0530

    update api ip for mobile testing

--- mobile/src/services/api/api_client.ts
+++ mobile/src/services/api/api_client.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 export const apiClient = axios.create({
-  baseURL: "https://studybuddy-backend-qq6l.onrender.com/",
-  // baseURL: "http://192.168.31.74:8000",
+  // baseURL: "https://studybuddy-backend-qq6l.onrender.com/",
+  baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
